672efcfbeb310fe6f3868caee5baa4cce2f79817
avner <avner.barr@gmail.com>
2021-10-31 09:19:52 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftBash",
-    platforms: [SupportedPlatform.macOS("13")],
+    platforms: [SupportedPlatform.macOS("12")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
